c720af5173ccb9c6e359f590efe97664357a4596
lucassantoscsa <150693349+lucassantoscsa@users.noreply.github.com>
2025-08-12 23:43:49 -0300

    sda

--- exercises/termo/solution/solution.py
+++ exercises/termo/solution/solution.py
@@ -1,11 +1,36 @@
-vencedor =[]
-for i in range(20):
-  piloto = input()
-  piloto = piloto.split(';')
-  piloto[3:] = [int(tempo) for tempo in piloto[3:]]
-  if sum(vencedor[3:])==0:
-      vencedor = piloto
-  elif sum(vencedor[3:])>sum(piloto[3:]):
-      vencedor = piloto
-vencedor[3:] = [str(tempo) for tempo in vencedor[3:]]
-print(",".join(vencedor))
+import random
+semente = input()
+semente = int(semente)
+random.seed(semente)
+palavras = [ 
+    "amigo", "certo", "livro", "carro", "terra", "festa", "piano", "gente",
+    "tempo", "nuvem", "casal", "fruto", "navio", "pedra", "roupa", "viver",
+    "cinto", "bravo", "leite", "sabor", "verde", "pente", "pleno", "vento",
+    "campo", "couro", "somar", "troca", "folha", "claro", "prato", "sede",
+    "firme", "cheio", "nobre", "areia", "lapis", "corte", "velho", "tocar",
+    "resto", "dente", "papel", "corpo", "beijo", "curva", "largo", "salto",
+    "farol", "falar", "linha", "forno", "muita", "natal", "vazio", "andar",
+    "chave", "casco", "signo", "trigo", "manga", "lenda", "salva", "luzir",
+    "ponto", "puxar", "fluir", "matar", "cansa", "dobro", "burro", "atual",
+    "morar", "garra", "morro", "clube", "ciclo", "bolsa", "marca", "junta",
+    "vasto", "perda", "poeta", "dizer", "lutar", "balde", "turma", "pular",
+    "norte", "sulco", "fugir", "curar", "risco", "arroz", "banco", "vinho",
+    "trono", "porta", "couro", "plano", "sinto", "cobre"
+]
+palavra = random.choice(palavras)
+venceu = False
+while venceu == False:
+    palpite = input()
+    if palpite == palavra:
+        venceu = True
+    else:
+        retorno = ''
+        for posicao, letra in enumerate(palpite):
+            if letra == palavra[posicao]:
+                retorno = retorno + '@'
+            elif letra in palavra:
+                retorno = retorno + '*'
+            else:
+                retorno = retorno + '_'
+        print(retorno)
+print(palavra)
